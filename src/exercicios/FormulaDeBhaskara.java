1de424d84c919bb1dc0345ba091f0587ca6111f5
Nicolas <nico124cunha@gmail.com>
2025-03-05 14:58:14 -0300

    Atividade Formula de Bhaskara

---
+++ src/exercicios/FormulaDeBhaskara.java
@@ -0,0 +1,23 @@
+package exercicios;
+import java.util.Scanner;
+
+public class FormulaDeBhaskara {
+    public static void main(String[] args) {
+        // Declaração de variáveis
+        Scanner ler = new Scanner(System.in);
+        double a = ler.nextDouble();
+        double b = ler.nextDouble();
+        double c = ler.nextDouble();
+        double delta = Math.pow(b,2.0) - 4 * a * c;
+        double x = (- b + Math.sqrt(delta)) / (2 * a);
+        double y = (- b - Math.sqrt(delta)) / (2 * a);
+
+        // Resolução do exercício
+        if (a == 0 || delta < 0){
+            System.out.println("Impossivel calcular");
+        }else {
+            System.out.printf("R1 = %.5f\n" , x);
+            System.out.printf("R2 = %.5f" , y);
+        }
+    }
+}
