570702f79e139acce944548fe4aa7cfda2461d4d
Nicolas <nico124cunha@gmail.com>
2025-03-11 14:29:00 -0300

    Meio do exercicio Media3

--- src/exercicios/iniciante/Media3.java
+++ src/exercicios/iniciante/Media3.java
@@ -1,9 +1,31 @@
 package exercicios.iniciante;
 
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
 public class Media3 {
     public static void main(String[] args) {
         // Declarando variáveis
-        double n1, n2, n3, n4;
+        Scanner nota = new Scanner(System.in);
+        DecimalFormat df = new DecimalFormat("0.0");
+        double n1 = nota.nextDouble(),
+                n2 = nota.nextDouble(),
+                n3 = nota.nextDouble(),
+                n4 = nota.nextDouble();
+        double media = ((n1*2) + (n2*3) + (n3*4) + (n4)) / 10;
+        // Resolução do exercício
+        System.out.println("Média: " + df.format(media));
+        if (media >= 7.0){
+            System.out.println("Aluno aprovado");
+        } else if (media < 5.0) {
+            System.out.println("Aluno em exame");
+            double exame = nota.nextDouble();
+            System.out.println("Nota do exame: " + df.format(exame));
+            media = (media + exame) / 2;
+            if (media >= 5){
+
+            }
+        }
 
     }
 }
