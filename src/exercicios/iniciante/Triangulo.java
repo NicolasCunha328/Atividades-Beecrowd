7211948fb496be21ef9bbc66383974667615c066
Nicolas Cunha <nico124cunha@gmail.com>
2025-03-31 20:10:36 -0300

    Triangulo

--- src/exercicios/iniciante/Triangulo.java
+++ src/exercicios/iniciante/Triangulo.java
@@ -1,7 +1,26 @@
 package exercicios.iniciante;
 
+import java.util.Scanner;
+
 public class Triangulo {
     public static void main(String[] args) {
+        // Definindo as variáveis
+        Scanner leitura = new Scanner(System.in);
+        double a = leitura.nextDouble();
+        double b = leitura.nextDouble();
+        double c = leitura.nextDouble();
+        double soma = a + b + c;
+        double area = (a + b) * c / 2;
 
+        // Resolução do exercício
+        if (a + b > c){
+            System.out.println("Perimetro = " + soma);
+        } else if (a + c > b) {
+            System.out.println("Perimetro = " + soma);
+        } else if (b + c > a) {
+            System.out.println("Perimetro = " + soma);
+        }else {
+            System.out.println("Area = " + area);
+        }
     }
 }
