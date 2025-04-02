079c6ef134fa93d413cfa3ef18eb9d4ab9d02043
Nicolas <nico124cunha@gmail.com>
2025-04-02 11:47:22 -0300

    Finalização exercício Tomadas

--- src/exercicios/iniciante/Tomadas.java
+++ src/exercicios/iniciante/Tomadas.java
@@ -13,10 +13,13 @@ public class Tomadas {
                 t2 = leitura.nextInt(),
                 t3 = leitura.nextInt(),
                 t4 = leitura.nextInt();
-        int st1, st2, st3, st4;
+        int st1 = t1 - 1,
+                st2 = t2 - 1,
+                st3 = t3 - 1,
+                soma = st1 + st2 + st3 + t4;
 
         if (2 <= t1 && 2 <= t2 && 2 <= t3 && 2 <= t4 && 6 >= t1 && 6 >= t2 && 6 >= t3 && 6 >= t4) {
-
+            System.out.println(soma);
         }
     }
 }
