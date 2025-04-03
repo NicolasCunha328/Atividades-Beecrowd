d34790426e05825c88c6da0ecf034f53f0980fef
Nicolas <nico124cunha@gmail.com>
2025-04-03 12:27:58 -0300

    Mudanças exercício Cedulas

--- src/exercicios/iniciante/Cedulas.java
+++ src/exercicios/iniciante/Cedulas.java
@@ -9,20 +9,19 @@ public class Cedulas {
         int n = numero.nextInt(); // Váriavel para o número digitado
 
         // Resolução do exercicio
-        System.out.println("Digite um número: " + n);
 
-        System.out.println(n / 100 + " nota(s) de R$ 100,00.");
+        System.out.println(n / 100 + " nota(s) de R$ 100,00");
         n = n % 100;
-        System.out.println(n / 50 + " nota(s) de R$ 50,00.");
+        System.out.println(n / 50 + " nota(s) de R$ 50,00");
         n = n % 50;
-        System.out.println(n / 20 + " nota(s) de R$ 20,00.");
+        System.out.println(n / 20 + " nota(s) de R$ 20,00");
         n = n % 20;
-        System.out.println(n / 10 + " nota(s) de R$ 10,00.");
+        System.out.println(n / 10 + " nota(s) de R$ 10,00");
         n = n % 10;
-        System.out.println(n / 5 + " nota(s) de R$ 5,00.");
+        System.out.println(n / 5 + " nota(s) de R$ 5,00");
         n = n % 5;
-        System.out.println(n / 2 + " nota(s) de R$ 2,00.");
+        System.out.println(n / 2 + " nota(s) de R$ 2,00");
         n = n % 2;
-        System.out.println(n + " nota(s) de R$ 1,00.");
+        System.out.println(n + " nota(s) de R$ 1,00");
     }
 }
