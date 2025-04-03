b9ec420a1e8a0d7ac32f81f5281868effad71bb8
Nicolas <nico124cunha@gmail.com>
2025-04-03 12:27:37 -0300

    Finalização exercício Tempo de Jogo

---
+++ src/exercicios/iniciante/TempoDeJogo.java
@@ -0,0 +1,22 @@
+package exercicios.iniciante;
+
+import java.util.Scanner;
+
+public class TempoDeJogo {
+    public static void main(String[] args) {
+        // Declaração das variáveis
+        Scanner ler = new Scanner(System.in);
+        int hi = ler.nextInt();
+        int hf = ler.nextInt();
+        int jogo; //  1 <= jogo <= 24
+
+        // Resolução do exercício
+        if (hi > hf){
+            System.out.println("O JOGO DUROU " + (24 - (hi - hf)) + " HORA(S)");
+        }else if (hf > hi){
+            System.out.println("O JOGO DUROU " + (hf - hi) + " HORA(S)");
+        }else {
+            System.out.println("O JOGO DUROU 24 HORA(S)");
+        }
+    }
+}
