a808d773b780069949643caf64548cfa03bc908d
Nicolas <nico124cunha@gmail.com>
2025-04-03 14:21:54 -0300

    Finalização exercício Tempo de jogo em minutos

--- src/exercicios/iniciante/TempoDeJogoComMinutos.java
+++ src/exercicios/iniciante/TempoDeJogoComMinutos.java
@@ -10,18 +10,21 @@ public class TempoDeJogoComMinutos {
                 mi = ler.nextInt(),
                 hf = ler.nextInt(),
                 mf = ler.nextInt();
-        int horas1 = 24 - (hi - hf);
-        int horas2 = hf - hi;
-        int min1 = (horas1 + mi + mf) * 60;
-        int min2 = ((horas2 + mi + mf) * 60) / 60;
+        int horas = hf - hi;
+        int minutos = mf - mi;
 
         // Resolução do exercício
-        if (hi > hf){
-            System.out.println((24 - (hi - hf)) + " HORA(S)");
-            System.out.println(min1 + " MINUTO(S)");
-        }else if (hf > hi){
-            System.out.println((hf - hi) + " HORA(S)");
-            System.out.println(min2+ " MINUTO(S)");
+        if (horas < 0){
+            horas = 24 + (hf - hi);
+        }
+        if (minutos < 0){
+            minutos = 60 + (mf - mi);
+            horas--;
+        }
+        if (hi == hf && mi == mf){
+            System.out.println("O JOGO DUROU 24 HORA(S) E 0 MINUTO(S)");
+        }else {
+            System.out.println("O JOGO DUROU " + horas + " HORA(S) E "+ minutos + " MINUTO(S)");
         }
     }
 }
