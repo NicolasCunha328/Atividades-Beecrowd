c652e5834e8faca53f27cb9b609fd798c84087bd
Nicolas <nico124cunha@gmail.com>
2025-04-08 17:01:35 -0300

    Finalização exercício Tempo de jogo em minutos

--- src/exercicios/iniciante/TempoDeJogoComMinutos.java
+++ src/exercicios/iniciante/TempoDeJogoComMinutos.java
@@ -4,27 +4,24 @@ import java.util.Scanner;
 
 public class TempoDeJogoComMinutos {
     public static void main(String[] args) {
-        // Declaração das variáveis
-        Scanner ler = new Scanner(System.in);
-        int hi = ler.nextInt(),
-                mi = ler.nextInt(),
-                hf = ler.nextInt(),
-                mf = ler.nextInt();
-        int horas = hf - hi;
-        int minutos = mf - mi;
-
-        // Resolução do exercício
-        if (horas < 0){
-            horas = 24 + (hf - hi);
-        }
-        if (minutos < 0){
-            minutos = 60 + (mf - mi);
-            horas--;
-        }
-        if (hi == hf && mi == mf){
-            System.out.println("O JOGO DUROU 24 HORA(S) E 0 MINUTO(S)");
+        Scanner entrada = new Scanner(System.in);
+        int h1 = entrada.nextInt();
+        int m1 = entrada.nextInt();
+        int h2 = entrada.nextInt();
+        int m2 = entrada.nextInt();
+        int mt1 = (h1*60) + m1;
+        int mt2 = (h2*60) + m2;
+        int h24 = (24*60);
+        int resultado;
+        if(mt2>mt1){
+            resultado = mt2-mt1;
+            System.out.printf("O JOGO DUROU %d HORA(S) E %d MINUTO(S)\n",(resultado/60), (resultado%60));
+        }else if(mt1>mt2){
+            resultado = (h24-mt1)+(mt2);
+            System.out.printf("O JOGO DUROU %d HORA(S) E %d MINUTO(S)\n",(resultado/60), (resultado%60));
         }else {
-            System.out.println("O JOGO DUROU " + horas + " HORA(S) E "+ minutos + " MINUTO(S)");
+            System.out.printf("O JOGO DUROU %d HORA(S) E %d MINUTO(S)\n",24, 0);
         }
+
     }
 }
