115cca22d9f6261d6e8ce952bcbae958110a3f01
Nicolas <nico124cunha@gmail.com>
2025-04-11 16:24:18 -0300

    Começando exercício Attack on Gasparini

---
+++ src/exercicios/iniciante/AttackOnGasparini.java
@@ -0,0 +1,11 @@
+package exercicios.iniciante;
+
+import java.util.Scanner;
+
+public class AttackOnGasparini {
+    public static void main(String[] args) {
+        //Declaração das variáveis
+        Scanner ler = new Scanner(System.in);
+
+    }
+}
